f19c8f8e0d711fd49e899e8ee839a685285d67a4
Viktor Mandlbauer <viktor.mandlbauer@protonmail.com>
2025-09-16 20:17:00 +0200

    backend project setup

---
+++ tayaki-backend/src/main/java/at/fhtw/tayaki/TayakiBackendApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.tayaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TayakiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TayakiBackendApplication.class, args);
+    }
+
+}
---
+++ tayaki-backend/src/test/java/at/fhtw/tayaki/TayakiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.tayaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TayakiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
