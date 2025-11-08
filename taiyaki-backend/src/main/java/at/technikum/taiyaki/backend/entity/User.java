4857d4d7626efd1fa93f27c08582e51d23f2634f
Viktor Mandlbauer <viktor.mandlbauer@protonmail.com>
2025-11-08 10:32:20 +0100

    added db entity

---
+++ taiyaki-backend/src/main/java/at/technikum/taiyaki/backend/entity/User.java
@@ -0,0 +1,27 @@
+package at.technikum.taiyaki.backend.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+}
